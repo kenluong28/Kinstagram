552e8207aa1aab223e876ee74628dc4ed4ea061b
Kenneth Luong <102771088+kenluong28@users.noreply.github.com>
2022-08-04 04:07:50 -0400

    compenents: header, image, action (like & comment)

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -1,11 +1,39 @@
 import { useRef } from "react";
 import PropTypes from "prop-types";
+import Header from "./header";
+import Image from "./image";
+import Actions from "./actions";
 
 export default function Post({ content }) {
-    console.log('content', content);
-    return <p>I am a post</p>;
+    const commentInput = useRef(null);
+    const handleFocus = () => commentInput.current.focus();
+
+    // components
+    // -> header, image, actions (like & comment icons), footer, comments
+
+    return (
+        <div className="rounded col-span-4 border bg-white border-gray-primary mb-12">
+            <Header username={content.username} />
+            <Image src={content.imageSrc} caption={content.caption} />
+            <Actions
+                docId={content.docId}
+                totalLikes={content.likes.length}
+                likedPhoto={content.userLikedPhoto}
+                handleFocus={handleFocus}
+            />
+        </div>
+    );
 }
 
 Post.propTypes = {
-    content: PropTypes.shape({})
+    content: PropTypes.shape({
+        username: PropTypes.string.isRequired,
+        imageSrc: PropTypes.string.isRequired,
+        caption: PropTypes.string.isRequired,
+        docId: PropTypes.string.isRequired,
+        userLikedPhoto: PropTypes.bool.isRequired,
+        likes: PropTypes.array.isRequired,
+        comments: PropTypes.array.isRequired,
+        dateCreated: PropTypes.number.isRequired
+    })
 };
